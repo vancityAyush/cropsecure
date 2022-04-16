148dec3015cf72e4377eb2ba30d04cc2b0ed3520
Ayush Kumar <eliasakeleven@gmail.com>
2022-04-16 13:59:21 +0530

    feedback form added

--- lib/screen/addfarmer/feedback_form.dart
+++ lib/screen/addfarmer/feedback_form.dart
@@ -21,15 +21,15 @@ class FeedbackForm extends StatefulWidget {
 class _State extends State<FeedbackForm> {
   TextEditingController _commentController = TextEditingController();
   bool isLoad = false;
-  RxInt plotDigitisation = 0.obs,
-      cropManagement = 0.obs,
-      specificCrop = 0.obs,
-      harvestQuality = 0.obs,
-      maskedLinkage = 0.obs,
-      farmLevelMeetings = 0.obs,
-      farmLevelAlert = 0.obs,
-      weatherReports = 0.obs,
-      welcome_sms = 0.obs;
+  RxInt plotDigitisation = 3.obs,
+      cropManagement = 3.obs,
+      specificCrop = 3.obs,
+      harvestQuality = 3.obs,
+      maskedLinkage = 3.obs,
+      farmLevelMeetings = 3.obs,
+      farmLevelAlert = 3.obs,
+      weatherReports = 3.obs,
+      welcome_sms = 3.obs;
 
   @override
   Widget build(BuildContext context) {
@@ -49,15 +49,15 @@ class _State extends State<FeedbackForm> {
             Get.back();
           },
         ),
-        toolbarHeight: 80,
+        toolbarHeight: 83,
       ),
       body: SingleChildScrollView(
         child: Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+          padding: const EdgeInsets.symmetric(horizontal: 23, vertical: 13),
           child: Column(
             children: [
               Container(
-                padding: const EdgeInsets.all(15.0),
+                padding: const EdgeInsets.all(15.3),
                 alignment: Alignment.center,
                 width: double.infinity,
                 child: const Text(
@@ -71,44 +71,44 @@ class _State extends State<FeedbackForm> {
               RatingWidget(
                   text: "Farmer Plot Digitisation", value: plotDigitisation),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(text: "Crop Management", value: cropManagement),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(text: "Specific Crop Advisory", value: specificCrop),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(
                   text: "Expected Harvest Quantity", value: harvestQuality),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(text: "Masked Linkage", value: maskedLinkage),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(
                   text: "Farm Level Meetings", value: farmLevelMeetings),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(text: "Farm Level Alerts", value: farmLevelAlert),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(text: "Weather Reports", value: weatherReports),
               const SizedBox(
-                height: 10,
+                height: 13,
               ),
               RatingWidget(text: "Welcome SMS", value: welcome_sms),
               Card(
                 child: Column(
                   children: [
                     Container(
-                      padding: const EdgeInsets.all(10),
+                      padding: const EdgeInsets.all(13),
                       child: TextField(
                         controller: _commentController,
                         decoration: const InputDecoration(
@@ -120,16 +120,16 @@ class _State extends State<FeedbackForm> {
                         keyboardType: TextInputType.multiline,
                       ),
                     ),
-                    const SizedBox(height: 20),
+                    const SizedBox(height: 23),
                     //Button for submit with rounded corners
                     Container(
-                      margin: const EdgeInsets.only(top: 20, bottom: 10),
+                      margin: const EdgeInsets.only(top: 23, bottom: 13),
                       child: RaisedButton(
                           color: Colors.green,
                           shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(10)),
+                              borderRadius: BorderRadius.circular(13)),
                           padding: const EdgeInsets.symmetric(
-                              vertical: 12, horizontal: 80),
+                              vertical: 12, horizontal: 83),
                           child: isLoad
                               ? const CircularProgressIndicator(
                                   color: Colors.white,
@@ -164,21 +164,21 @@ class _State extends State<FeedbackForm> {
                             setState(() {
                               isLoad = false;
                               _commentController.clear();
-                              plotDigitisation.value = 0;
-                              cropManagement.value = 0;
-                              specificCrop.value = 0;
-                              harvestQuality.value = 0;
-                              maskedLinkage.value = 0;
-                              farmLevelMeetings.value = 0;
-                              farmLevelAlert.value = 0;
-                              weatherReports.value = 0;
-                              welcome_sms.value = 0;
+                              plotDigitisation.value = 3;
+                              cropManagement.value = 3;
+                              specificCrop.value = 3;
+                              harvestQuality.value = 3;
+                              maskedLinkage.value = 3;
+                              farmLevelMeetings.value = 3;
+                              farmLevelAlert.value = 3;
+                              weatherReports.value = 3;
+                              welcome_sms.value = 3;
                             });
                           }),
                     ),
                   ],
                 ),
-                margin: const EdgeInsets.all(20),
+                margin: const EdgeInsets.all(23),
               ),
             ],
           ),
@@ -197,7 +197,7 @@ class RatingWidget extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Container(
-      padding: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(13),
       margin: const EdgeInsets.all(5),
       decoration: BoxDecoration(
         borderRadius: BorderRadius.circular(8),
@@ -227,7 +227,7 @@ class RatingWidget extends StatelessWidget {
                 direction: Axis.horizontal,
                 allowHalfRating: false,
                 itemCount: 4,
-                itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+                itemPadding: const EdgeInsets.symmetric(horizontal: 4.3),
                 itemBuilder: (context, index) => const Icon(
                   Icons.star,
                   color: Colors.amber,
